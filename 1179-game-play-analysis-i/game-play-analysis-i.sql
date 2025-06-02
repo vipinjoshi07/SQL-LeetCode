afa44c3436a59987cfc828b06ef377aa8d036610
vipinjoshi07 <78019046+vipinjoshi07@users.noreply.github.com>
2025-06-02 16:09:53 +0530

    Time: 485 ms (71.15%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
-SELECT player_id, MIN(event_date) AS first_login
-FROM Activity
-GROUP BY player_id
-ORDER BY player_id;
+WITH ranked_logins AS (
+    SELECT player_id, event_date,
+    ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date ASC) AS rn
+    FROM Activity
+)
+
+SELECT player_id, event_date AS first_login
+FROM ranked_logins
+WHERE rn = 1
